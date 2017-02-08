4c0f3e778ef14f751ac0114eb3d813e03992e1c5
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-02-08 13:33:19 -0600

    added crashlytics, canaryleak, and GSON dependencies

--- BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/AppController.java
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/AppController.java
@@ -5,12 +5,28 @@ import android.app.Application;
 /**
  * Created by sharukhhasan on 2/8/17.
  */
-
 public class AppController extends Application {
+
+    //public static RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        /*StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDialog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll()
+                .penaltyLog()
+                .build());*/
+
+        //if (LeakCanary.isInAnalyzerProcess(this)) {
+        // This process is dedicated to LeakCanary for heap analysis.
+        // You should not init your app in this process.
+        //return;
+        //}
+        //refWatcher = LeakCanary.install(this);
     }
 }
