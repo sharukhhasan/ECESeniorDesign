52b6ac0807ee9de9dab87fac0126e9cab81f66f0
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-02-08 13:51:31 -0600

    added adapter for devices when user is prompted to connect to ble device

---
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/adapters/DeviceListAdapter.java
@@ -0,0 +1,79 @@
+package com.hrl.bluetoothlowenergy.adapters;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import com.hrl.bluetoothlowenergy.R;
+
+/**
+ * Created by Sharukh Hasan on 7/20/16.
+ * Copyright © 2016 Coapt Engineering. All rights reserved.
+ */
+public class DeviceListAdapter extends ArrayAdapter<BluetoothDevice> {
+    private Context context;
+    private final List<BluetoothDevice> deviceItems;
+    private BluetoothAdapter bTAdapter;
+
+    public DeviceListAdapter(Context context, List<BluetoothDevice> deviceItems, BluetoothAdapter bTAdapter) {
+        super(context, android.R.layout.simple_list_item_1, deviceItems);
+        this.bTAdapter = bTAdapter;
+        this.deviceItems = deviceItems;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return deviceItems.size();
+    }
+
+    @Override
+    public BluetoothDevice getItem(int position) {
+        return deviceItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder{
+        TextView titleText;
+        TextView macText;
+
+        private ViewHolder(View view){
+            titleText = (TextView) view.findViewById(R.id.coaptDeviceName);
+            macText = (TextView) view.findViewById(R.id.coaptDeviceAddress);
+        }
+    }
+
+    @NonNull
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = convertView;
+        ViewHolder holder;
+
+        if(view != null) {
+            holder = (ViewHolder) view.getTag();
+        } else {
+            view = inflater.inflate(R.layout.device_view, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        }
+
+        BluetoothDevice device = deviceItems.get(position);
+        holder.titleText.setText(device.getName());
+        holder.macText.setText(device.getAddress());
+
+        return view;
+    }
+}
