eff0735df24096bf20c557aee38620b08ae93337
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-02-08 13:50:59 -0600

    added main activity

---
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hrl.bluetoothlowenergy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hrl.bluetoothlowenergy.R;
+
+/**
+ * Created by Sharukh Hasan on 2/8/16.
+ *
+ * Main activity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
