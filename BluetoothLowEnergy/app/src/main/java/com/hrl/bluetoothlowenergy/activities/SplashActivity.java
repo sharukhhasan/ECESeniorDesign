e80f6e3b989eb64335da06dc9149f3d3825d449b
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-02-08 13:38:29 -0600

    packaging for activities

---
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/activities/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.hrl.bluetoothlowenergy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
+import com.hrl.bluetoothlowenergy.R;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by sharukhhasan on 2/8/17.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_splash);
+    }
+}
