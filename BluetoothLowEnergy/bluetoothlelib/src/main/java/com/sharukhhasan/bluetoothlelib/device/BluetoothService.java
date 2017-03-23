56f2120e9591971ed66b2faa55175bd2588efdd1
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-03-23 18:51:22 -0500

    implemented ble device and service

--- BluetoothLowEnergy/bluetoothlelib/src/main/java/com/sharukhhasan/bluetoothlelib/device/BluetoothService.java
+++ BluetoothLowEnergy/bluetoothlelib/src/main/java/com/sharukhhasan/bluetoothlelib/device/BluetoothService.java
@@ -1,8 +1,29 @@
 package com.sharukhhasan.bluetoothlelib.device;
 
-/**
- * Created by sharukhhasan on 3/23/17.
- */
+import android.bluetooth.BluetoothClass;
 
-public class BluetoothService {
+/**
+ * Created by Sharukh Hasan on 3/23/17.
+ *
+ */
+public enum BluetoothService {
+    AUDIO(BluetoothClass.Service.AUDIO),
+    CAPTURE(BluetoothClass.Service.CAPTURE),
+    INFORMATION(BluetoothClass.Service.INFORMATION),
+    LIMITED_DISCOVERABILITY(BluetoothClass.Service.LIMITED_DISCOVERABILITY),
+    NETWORKING(BluetoothClass.Service.NETWORKING),
+    OBJECT_TRANSFER(BluetoothClass.Service.OBJECT_TRANSFER),
+    POSITIONING(BluetoothClass.Service.POSITIONING),
+    RENDER(BluetoothClass.Service.RENDER),
+    TELEPHONY(BluetoothClass.Service.TELEPHONY);
+
+    private final int mAndroidConstant;
+
+    BluetoothService(final int androidCode){
+        mAndroidConstant = androidCode;
+    }
+
+    public int getAndroidConstant(){
+        return mAndroidConstant;
+    }
 }
