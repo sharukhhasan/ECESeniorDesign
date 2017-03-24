8960da50e2ce8e56c0606c62f750b6faec5599c1
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-03-23 19:01:36 -0500

    interfaces for beacon classes

---
+++ BluetoothLowEnergy/bluetoothlelib/src/main/java/com/sharukhhasan/bluetoothlelib/device/beacon/ibeacon/IBeaconDistanceDescriptor.java
@@ -0,0 +1,12 @@
+package com.sharukhhasan.bluetoothlelib.device.beacon.ibeacon;
+
+/**
+ * Created by Sharukh Hasan on 3/23/17.
+ *
+ */
+public enum IBeaconDistanceDescriptor {
+    IMMEDIATE,
+    NEAR,
+    FAR,
+    UNKNOWN,
+}
