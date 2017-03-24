185cfb62cde58e8d640347a01c543b7a381953f8
sharukhhasan <sharukh-hasan@uiowa.edu>
2017-03-24 13:32:03 -0500

    separated global constants into util

---
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.hrl.bluetoothlowenergy.utils;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by sharukhhasan on 3/24/17.
+ */
+
+public class Constants {
+
+    public static final DecimalFormat DOUBLE_TWO_DIGIT_ACCURACY = new DecimalFormat("#.##");
+    public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}
