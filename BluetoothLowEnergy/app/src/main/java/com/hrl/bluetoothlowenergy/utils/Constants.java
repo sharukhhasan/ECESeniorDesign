f50f437654edf3d1f9a3e94031837719ee8259a8
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2017-04-23 18:44:03 -0500

    more work towards bluetooth on android side

--- BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/utils/Constants.java
+++ BluetoothLowEnergy/app/src/main/java/com/hrl/bluetoothlowenergy/utils/Constants.java
@@ -10,4 +10,13 @@ public class Constants {
 
     public static final DecimalFormat DOUBLE_TWO_DIGIT_ACCURACY = new DecimalFormat("#.##");
     public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
 }
