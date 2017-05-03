83ac938f80503c26085be81331f3406382685c7c
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2017-05-02 19:45:38 -0500

    replacing main activity

---
+++ TractorVision/app/src/main/java/com/hrl/tractorvision/activity/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.hrl.tractorvision.activity;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.hrl.tractorvision.R;
+import com.hrl.tractorvision.fragment.MainFragment;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG_MAIN_FRAGMENT = "MainFragment";
+    private String LOG_TAG = "MainActivity:";
+
+    private MainFragment mMainFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        mMainFragment = (MainFragment) fm.findFragmentByTag(TAG_MAIN_FRAGMENT);
+
+        //create the fragment on the first run
+        if (mMainFragment == null) {
+            Log.v(LOG_TAG, "creating new MainFragment");
+            //if (mMainFragment == null) {
+            mMainFragment = new MainFragment();
+            fm.beginTransaction().replace(R.id.gui_container, mMainFragment, TAG_MAIN_FRAGMENT).commit();
+
+
+            //FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            //fragmentTransaction.replace(R.id.gui_container, mMainFragment).commit();
+        }
+    }
+}
